a34c9ea700f63d5e40e64ab09223e1af8f0e7cb3
Ryan Taylor <ryan@beta-reduce.net>
2024-05-17 18:23:26 -0400

    fixes

--- src/data/download_data.py
+++ src/data/download_data.py
@@ -10,13 +10,7 @@ enc_base = tiktoken.get_encoding("cl100k_base")
 enc = tiktoken.Encoding(
     name="cl100k_base",
     pat_str=enc_base._pat_str,
-    mergeable_ranks=enc_base._mergeable_ranks,
-    special_tokens={
-        **enc_base._special_tokens,
-        "<|im_start|>": 100264,
-        "<|im_end|>": 100265,
-        "<|pad|>": 100263
-    }
+    mergeable_ranks=enc_base._mergeable_ranks
 )
 
 dataset_starcoder = load_dataset("bigcode/starcoderdata", streaming=True)
