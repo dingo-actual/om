dc98703676ea4e2da1e9044318f8ab15fb6339ae
Ryan Taylor <ryan@beta-reduce.net>
2024-05-18 14:55:34 -0400

    implemented model

---
+++ src/om_llm/model.py
@@ -0,0 +1,82 @@
+from typing import List, Optional
+
+import torch
+
+from ..arcformer import ARCformer, RoPEEmbeddings
+
+
+class OmLLM(torch.nn.Module):
+    def __init__(
+        self,
+        num_layers: int,
+        vocab_size: int,
+        dim_input: int,
+        dim_hidden: int,
+        dims_key: List[int],
+        dims_value: List[int],
+        mem_iters: int,
+        num_heads: int,
+        activation: str,
+        segment_len: int,
+        state_len: int,
+        normalize_qkv: bool,
+        position_embedders: List[Optional[RoPEEmbeddings]],
+        dropout: float = 0.0,
+        init_conv: bool = False,
+        device: Optional[str] = None
+    ):
+        super(OmLLM, self).__init__()
+        
+        self.embedder = torch.nn.Embedding(vocab_size, dim_input, device=device)
+        
+        layers = [
+            ARCformer(
+                dim_input=dim_input,
+                dim_hidden=dim_hidden,
+                dims_key=dims_key,
+                dims_value=dims_value,
+                mem_iters=mem_iters,
+                num_heads=num_heads,
+                activation=activation,
+                segment_len=segment_len,
+                state_len=state_len,
+                normalize_qkv=normalize_qkv,
+                position_embedders=position_embedders,
+                dropout=dropout,
+                init_conv=init_conv,
+                device=device
+            )
+        ]
+        for _ in range(num_layers - 1):
+            layers.append(
+                ARCformer(
+                    dim_input=dim_input,
+                    dim_hidden=dim_hidden,
+                    dims_key=dims_key,
+                    dims_value=dims_value,
+                    mem_iters=mem_iters,
+                    num_heads=num_heads,
+                    activation=activation,
+                    segment_len=segment_len,
+                    state_len=state_len,
+                    normalize_qkv=normalize_qkv,
+                    position_embedders=position_embedders,
+                    dropout=dropout,
+                    init_conv=False,
+                    device=device
+                )
+            )
+        self.layers = torch.nn.ModuleList(layers).to(device=device)
+        self.proj_out = torch.nn.Linear(dim_input, vocab_size, device=device)
+        
+    def get_logits(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.embedder(x)
+        for layer in self.layers:
+            x = layer(x)
+        x = self.proj_out(x)
+        
+        return x
+    
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        logits = self.get_logits(x)
+        return torch.nn.functional.sigmoid(logits)
