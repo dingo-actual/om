2bb689e4d570edbfda18739d16dad2b54bc00051
Ryan Taylor <ryan@beta-reduce.net>
2024-05-26 11:53:57 +0000

    changed tokenizer to p50k_edit

--- src/data/tokenizer.py
+++ src/data/tokenizer.py
@@ -1,13 +1,13 @@
 import tiktoken
 
-enc_base = tiktoken.get_encoding("cl100k_base")
+enc_base = tiktoken.get_encoding("p50k_edit")
 enc = tiktoken.Encoding(
-    name="cl100k_base",
+    name="p50k_edit",
     pat_str=enc_base._pat_str,
     mergeable_ranks=enc_base._mergeable_ranks,
     special_tokens={
-        "<|im_start|>": 100257,
-        "<|im_end|>": 100258,
-        "<|pad|>": 100259,
+        "<|im_start|>": 50281,
+        "<|im_end|>": 50256,
+        "<|pad|>": 50282,
     }
 )
