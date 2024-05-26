4c7406b4a5fc068a13793514d3f6812b3417851a
Ryan Taylor <ryan@beta-reduce.net>
2024-05-26 11:32:43 +0000

    removed deprecated

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,4 +1,4 @@
-from .datasets import ParquetFilesDataset, CompressedJSONLFilesDataset, TokenizedDataset, ProportionalDataset
+from .datasets import ParquetFilesDataset, CompressedJSONLFilesDataset, ProportionalDataset
 from .util import get_dataset_stage, get_datasets_stages
 from .tokenizer import enc
 
@@ -6,7 +6,6 @@ from .tokenizer import enc
 __all__ = [
     "ParquetFilesDataset",
     "CompressedJSONLFilesDataset",
-    "TokenizedDataset",
     "ProportionalDataset",
     "get_dataset_stage",
     "get_datasets_stages",
