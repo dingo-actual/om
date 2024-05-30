b5f2797fd962e1029b4d2e25f4eacb31bf115b4e
Ryan Taylor <ryan@beta-reduce.net>
2024-05-30 13:35:27 +0000

    completed core train script

---
+++ src/train/__init__.py
@@ -0,0 +1,2 @@
+from .train_stage import train_stage
+from .eval_net import eval_net
---
+++ src/train/eval_net.py
@@ -0,0 +1,59 @@
+from accelerate import Accelerator
+from schedulefree import AdamWScheduleFree
+import torch
+from torch.utils.data import DataLoader
+from torch.utils.tensorboard.writer import SummaryWriter
+from torchmetrics.text import Perplexity
+
+from ..om_llm import OmLLM
+
+
+def eval_net(
+    model: OmLLM, 
+    optimizer: AdamWScheduleFree, 
+    loss_fn: torch.nn.Module,
+    perpelxity: Perplexity,
+    dataloader_eval: DataLoader, 
+    num_steps: int, 
+    accelerator: Accelerator,
+    writer: SummaryWriter,
+    batch_ix: int
+) -> None:
+    model = model.eval()
+    optimizer = optimizer.eval()
+    
+    loss_total = 0.0
+    pplx_total = 0.0
+    n_tokens = 0
+    
+    with torch.no_grad():
+        for step, batch in enumerate(dataloader_eval):
+            if step == num_steps:
+                break
+            
+            inputs = batch[:, :-1]
+            targets = batch[:, 1:]
+            
+            pred = model(inputs)
+            
+            with accelerator.autocast():
+                loss = loss_fn(pred, targets)
+                pplx = perpelxity(pred, targets)
+                
+            batch_tokens = batch.size(0) * batch.size(1)
+            loss_total += loss.cpu().detach().item() * batch_tokens
+            pplx_total += pplx.cpu().detach().item() * batch_tokens
+            n_tokens += batch_tokens
+            
+        accelerator.wait_for_everyone()
+        
+        eval_loss = loss_total / n_tokens
+        eval_pplx = pplx_total / n_tokens
+        
+        writer.add_scalar("Loss/Train", eval_loss, batch_ix)
+        writer.add_scalar("Perplexity/Train", eval_pplx, batch_ix)
+            
+    model.train()
+    optimizer.train()
+
+    return None
