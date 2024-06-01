facb2e73d1b97c4b9e5fd064962feabf3df91408
Ryan Taylor <ryan@beta-reduce.net>
2024-06-01 21:03:37 +0000

    bf16 -> fp16

--- src/train/train_stage.py
+++ src/train/train_stage.py
@@ -40,7 +40,7 @@ def train_stage(
     
     accelerator = Accelerator(
         project_dir=CHECKPOINT_DIR_STAGE, 
-        mixed_precision="bf16", 
+        mixed_precision="fp16", 
         gradient_accumulation_steps=gradient_accumulation_steps
     )
     
@@ -68,23 +68,24 @@ def train_stage(
     optimizer = optimizer.train()
     
     for batch_ix, batch in enumerate(dataloader_train):
-        optimizer.zero_grad()
-        
-        inputs = batch[:, :-1]
-        targets = batch[:, 1:]
-        
-        tokens_processed += batch.size(0) * batch.size(1)
-        
-        preds, _, _ = model(inputs)
-        
-        with accelerator.autocast():
-            loss = loss_fn(preds, targets)
-        
-        accelerator.backward(loss)
-        
-        if accelerator.sync_gradients:
-            accelerator.clip_grad_value_(model.parameters(), 1.0)
-        optimizer.step()
+        with accelerator.accumulate(model):
+            optimizer.zero_grad()
+            
+            inputs = batch[:, :-1]
+            targets = batch[:, 1:]
+            
+            tokens_processed += batch.size(0) * batch.size(1)
+            
+            preds, _, _ = model(inputs)
+            
+            with accelerator.autocast():
+                loss = loss_fn(preds, targets)
+            
+            accelerator.backward(loss)
+            
+            if accelerator.sync_gradients:
+                accelerator.clip_grad_value_(model.parameters(), 1.0)
+            optimizer.step()
         
         time_crnt = datetime.datetime.now()
         
