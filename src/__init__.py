adbc8e5bd0858519bc158994182a31e895c0e43f
Ryan Taylor <ryan@beta-reduce.net>
2024-06-03 00:00:00 +0000

    added imports, namespace

--- src/__init__.py
+++ src/__init__.py
@@ -1,2 +1,14 @@
-if __name__=="__main__":
-    pass
+from .arcformer import RoPEEmbeddings
+from .data import FilesDataset, ProportionalDataset, get_datasets_stages
+from .train import train_stage, eval_net
+from .om_llm import OmLLM
+
+__all__ = [
+    "FilesDataset",
+    "ProportionalDataset",
+    "get_datasets_stages",
+    "train_stage",
+    "eval_net",
+    "OmLLM",
+    "RoPEEmbeddings"
+]
