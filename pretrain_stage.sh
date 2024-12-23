784dc2e081af66e99feb1d5d61c9a9b1b11462ad
Ryan Taylor <ryan@beta-reduce.net>
2024-12-23 14:08:43 -0500

    minor pretrain improvements

--- pretrain_stage.sh
+++ pretrain_stage.sh
@@ -1,1 +1,3 @@
+mkdir -p /home/ubuntu/om-llm/om/runs
+tensorboard --logdir /home/ubuntu/om-llm/om/runs &
 accelerate launch --config-path=accelerate_config.yml pretrain_stage.py --config_dir=/home/ubuntu/om-llm/om/config
