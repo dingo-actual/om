9f87700f299fcc75c3b130a4b6d7ecd5e219d953
Ryan Taylor <ryan@beta-reduce.net>
2025-01-04 10:17:45 -0500

    remove unnecessary pip installs

--- pretrain_stage.sh
+++ pretrain_stage.sh
@@ -1,4 +1,2 @@
-pip install torch==2.4.1 --index-url https://download.pytorch.org/whl/cu124
-pip install xformers==0.0.28
 tensorboard --logdir /home/ubuntu/om-llm/runs --host 0.0.0.0 &
-accelerate launch --config_file=accelerate_config.yml pretrain_stage.py --config_dir=/home/ubuntu/om-llm/om/config
+accelerate launch --config_file=accelerate_config.yml pretrain_stage.py --config_dir=/home/ubuntu/om-llm/om/config &
