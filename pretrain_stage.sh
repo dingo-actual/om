26848148d37fcf62cf4b26c9a61f95c25dd2a0f5
Ryan Taylor <ryan@beta-reduce.net>
2025-01-04 10:16:42 -0500

    fix pytorch and xformers versions in requirements.txt

--- pretrain_stage.sh
+++ pretrain_stage.sh
@@ -1,4 +1,4 @@
-pip install torch==2.4.1 torchvision==0.19.1 torchaudio==2.4.1 --index-url https://download.pytorch.org/whl/cu124
+pip install torch==2.4.1 --index-url https://download.pytorch.org/whl/cu124
 pip install xformers==0.0.28
 tensorboard --logdir /home/ubuntu/om-llm/runs --host 0.0.0.0 &
 accelerate launch --config_file=accelerate_config.yml pretrain_stage.py --config_dir=/home/ubuntu/om-llm/om/config
