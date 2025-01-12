eb08f87b701d7250b0cd97d3cc42156c0153e8aa
Ryan Taylor <ryan@beta-reduce.net>
2025-01-12 12:55:04 -0500

    use correct tensorboard dir

--- pretrain_stage.sh
+++ pretrain_stage.sh
@@ -1,2 +1,2 @@
-tensorboard --logdir /home/ubuntu/om-llm/runs --host 0.0.0.0 &
+tensorboard --logdir /home/ubuntu/om-llm/checkpoints --host 0.0.0.0 &
 accelerate launch --config_file=accelerate_config.yml pretrain_stage.py --config_dir=/home/ubuntu/om-llm/om/config &
