63640b3b92535a27ac966ee6a6b5f9c0eb2e2493
Ryan Taylor <ryan@beta-reduce.net>
2025-03-08 08:40:09 -0500

    changed special token ids

--- src/om/data/tokenizer.py
+++ src/om/data/tokenizer.py
@@ -1,13 +1,14 @@
 import tiktoken
 
 enc_base = tiktoken.get_encoding("gpt2")
+
 enc = tiktoken.Encoding(
     name="gpt2",
     pat_str=enc_base._pat_str,
     mergeable_ranks=enc_base._mergeable_ranks,
     special_tokens={
-        "<|im_start|>": 50281,
+        "<|im_start|>": 50257,
         "<|im_end|>": 50256,
-        "<|pad|>": 50282,
+        "<|pad|>": 50258
     }
 )
